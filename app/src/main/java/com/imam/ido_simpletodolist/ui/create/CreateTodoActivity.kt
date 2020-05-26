acf49cbbd419da5a2a2d3b46cadca210f45c19e1
nurimammasri <nurimammasri.01@gmail.com>
2020-05-26 20:20:52 +0800

    RecyclerView and Update View (List View Item, Edit/View/Create Layout)

---
+++ app/src/main/java/com/imam/ido_simpletodolist/ui/create/CreateTodoActivity.kt
@@ -0,0 +1,13 @@
+package com.imam.ido_simpletodolist.ui.create
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.imam.ido_simpletodolist.R
+
+class CreateTodoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_todo)
+    }
+}
